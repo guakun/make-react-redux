4fba89b44281f3229abc49eb99bd7a64e9d5b956
wanghongkun <wanghongkun@corp.netease.com>
2018-07-13 16:00:35 +0800

    将土味 redux 更换为官版 redux & react-redux

--- src/index.js
+++ src/index.js
@@ -1,34 +1,17 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import {createStore} from 'redux'
+import {Provider} from "react-redux"
 import Header from './Header'
 import Content from './Content'
+
+import './index.css'
 import registerServiceWorker from './registerServiceWorker'
-import {Provider} from "./react-redux"
-
-function createStore(reducer) {
-  let state = null
-  const listeners = []
-  const subscribe = ((listener) => {
-    listeners.push(listener)
-  })
-
-  const getState = () => state
-
-  const dispatch = (action) => {
-    state = reducer(state, action)
-    listeners.forEach(listener => listener())
-  }
-
-  dispatch({})
-
-  return {getState, dispatch, subscribe}
-}
 
 const themeReducer = (state, action) => {
   if (!state) {
     return {
-      themeColor: 'red'
+      themeColor: 'green'
     }
   }
   switch (action.type) {
@@ -43,13 +26,15 @@ const store = createStore(themeReducer)
 class Index extends Component {
   render() {
     return (
-      <div>
-        <Header/>
-        <Content/>
-      </div>
+      <Provider store={store}>
+        <div>
+          <Header/>
+          <Content/>
+        </div>
+      </Provider>
     )
   }
 }
 
-ReactDOM.render(<Provider store={store}> <Index/> </Provider>, document.getElementById('root'))
+ReactDOM.render(<Index />, document.getElementById('root'))
 registerServiceWorker()
